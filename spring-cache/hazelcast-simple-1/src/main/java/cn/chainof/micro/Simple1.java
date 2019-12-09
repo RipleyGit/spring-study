009eaf24158443fd153705c6b28d3e59c52612f5
yubs <yubs@bsfit.com.cn>
2019-12-09 15:19:00 +0800

    spring集成hazelcast分布式缓存

--- spring-cache/hazelcast-simple-1/src/main/java/cn/chainof/micro/Simple1.java
+++ spring-cache/hazelcast-simple-1/src/main/java/cn/chainof/micro/Simple1.java
@@ -4,10 +4,8 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class Simple1 implements CommandLineRunner {
 
     public static void main(String[] args) {
